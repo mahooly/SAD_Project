2f1d4c29d91b53aa60fd1cc698cd51559b7b2bbe
Mahtab Movahedian <mahtab.movahedian@gmail.com>
2018-07-21 00:00:53 +0430

    Finished submit project

--- users/choices.py
+++ users/choices.py
@@ -3,9 +3,6 @@ SEX_CHOICES = (('male', 'Male'), ('female', 'Female'), ('other', 'Other'), ('bla
 EDUCATION_CHOICES = (
 ('nothing', 'nothing'), ('diploma', 'دیپلم و فوق دیپلم'), ('bachelor', 'لیسانس'), ('master', 'فوق لیسانس'), ('phd', 'دکترا'))
 
-CITY_CHOICES = (
-('blank', 'blank'), ('تهران', 'Tehran'), ('مشهد', 'Mashhad'), ('اصفهان', 'Isfahan'), ('شیراز', 'Shairaz'))
-
 BANK_CHOICES = (('card', '1'), ('account', '2'))
 
 COOP_CHOICES = (('inOffice10', 'inOffice10'), ('inOffice20', 'inOffice20'), ('inOffice30', 'inOffice30'), ('inOfficemore', 'inOfficemore'), ('atHome', 'atHome'))
